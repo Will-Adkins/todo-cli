3b53ee101a4ffed1af8eb9cfefeeea6d9975c197
Will Adkins <wkadki01@gmail.com>
2018-06-07 16:39:29 -0400

    handleCommand basic switch functionality

--- todo.js
+++ todo.js
@@ -1,5 +1,19 @@
-const R = require('ramda')
+const { head } = require('ramda')
 
-const stuff = ['foo', 2, { name: 'Will' }]
+const [execPath, jsPath, cmd, ...rest] = process.argv
 
-console.log(R.head(stuff))
+console.log(handleCommand(cmd))
+
+function handleCommand(cmd) {
+  // init, rm, add commands
+  switch (cmd) {
+    case 'add':
+      return 'asked for add cmd'
+    case 'rm':
+      return 'asked for rm cmd'
+    case 'init':
+      return 'asked for init cmd'
+    default:
+      return `${cmd} is not supported`
+  }
+}
